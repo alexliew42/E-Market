5f7edaf35b963e3a06cefb2e262f52ef14376773
Alex Liew <liewtwoo42@gmail.com>
2023-06-29 16:43:11 -0400

    add styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,11 @@
 export function Header() {
   return(
     <div id="navbar">
-      
+      <p id="logo">E-Market</p>
+      <nav id="menu">
+        <p>Home</p> 
+          
+      </nav>
     </div>
   )
 }
